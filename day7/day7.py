cd49ff582176fef1af30f0d08dd5537149a94df9
Alexander Dahl <alex@dahl.dev>
2020-12-07 23:07:34 +0100

    solve day day7b

--- day7/day7.py
+++ day7/day7.py
@@ -1,5 +1,7 @@
 def find_all_paths(graph, start, end, path=[]):
     path = path + [start]
+    if start != "shiny gold":
+        start = start[2:]
     if start == end:
         return True
     for node in graph[start]:
@@ -23,7 +25,15 @@ database = file.read().splitlines()
 # faded blue bags contain no other bags.
 # dotted black bags contain no other bags.
 # """
-# datebase = database.splitlines()
+# database = """shiny gold bags contain 2 dark red bags.
+# dark red bags contain 2 dark orange bags.
+# dark orange bags contain 2 dark yellow bags.
+# dark yellow bags contain 2 dark green bags.
+# dark green bags contain 2 dark blue bags.
+# dark blue bags contain 2 dark violet bags.
+# dark violet bags contain no other bags.
+# """
+# database = database.splitlines()
 bag_graph = {}
 
 # shiny gold bag is the root
@@ -35,15 +45,34 @@ for row in database:
         bag_graph[key] = ""
     else:
         value = split_str[1].replace(" bags", "").replace(" bag", "")
-        bag_graph[key] = [val[2:] for val in value.split(", ")]
+        # bag_graph[key] = [val[2:] for val in value.split(", ")]
+        bag_graph[key] = value.split(", ")
 
 for dic in bag_graph.items():
     print(dic)
 
-num_paths = 0
-for key_bag in bag_graph.keys():
-    if find_all_paths(bag_graph, key_bag, "shiny gold") and key_bag != "shiny gold":
-        print(key_bag + " can reach golden")
-        num_paths += 1
+# num_paths = 0
+# # part 1
+# for key_bag in bag_graph.keys():
+#     if find_all_paths(bag_graph, key_bag, "shiny gold") and key_bag != "shiny gold":
+#         print(key_bag + " can reach golden")
+#         num_paths += 1
 
-print(num_paths)
+# print(num_paths)
+
+
+# part 2
+def bag_count(graph, start):
+    count = 1
+    num_bags = 1
+
+    for bag in graph[start]:
+        if bag != "shiny gold":
+            num_bags = int(bag[0])
+            next_bag = bag[2:]
+            print(bag)
+            count += num_bags * bag_count(graph, next_bag)
+    return count
+
+
+print(bag_count(bag_graph, "shiny gold") - 1)
