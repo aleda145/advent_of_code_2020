58203ba408e7915bc6f471eff997856dfe5ca9d7
Alexander Dahl <alex@dahl.dev>
2020-12-23 12:42:21 +0100

    solve 15b

--- day15/day15.py
+++ day15/day15.py
@@ -1,33 +1,31 @@
+from collections import defaultdict
+
 numbers = [0, 3, 6]
-numbers = [1, 3, 2]
 numbers = [11, 18, 0, 20, 1, 7, 16]
+# numbers = [3, 2, 1]
 spoken_numbers = []
+spoken_num_occ = defaultdict(int)
+spoken_num_turns = defaultdict(list)
 turn = 1
 last_num = 0
 while True:
     if turn <= len(numbers):
         num = numbers[turn - 1]
-        spoken_numbers.append(num)
+        spoken_num_turns[num].append(turn)
     else:
         # done all start nums
-        if spoken_numbers.count(last_num) == 1:
+        if spoken_num_occ[last_num] == 1:
             num = 0
-            spoken_numbers.append(num)
         else:
-            last_spoken_turn = None
-            last_last_spoken_turn = None
-            for index, spoken_number in enumerate(reversed(spoken_numbers)):
-                if spoken_number == last_num and not last_spoken_turn:
-                    last_spoken_turn = turn - (index + 1)
-                    continue
-                if last_spoken_turn and spoken_number == last_num:
-                    last_last_spoken_turn = turn - (index + 1)
-                    break
+            seen_turns = spoken_num_turns[last_num]
+            last_spoken_turn = seen_turns[-1]
+            last_last_spoken_turn = seen_turns[-2]
             num = last_spoken_turn - last_last_spoken_turn
-            spoken_numbers.append(num)
-
-    print(f"turn: {turn}: {num}")
+    spoken_num_turns[num].append(turn)
+    spoken_num_occ[num] += 1
+    if turn % 100000 == 0:
+        print(f"turn: {turn}: {num}")
     last_num = num
-    if turn >= 2020:
+    if turn >= 30000000:
         break
     turn += 1
