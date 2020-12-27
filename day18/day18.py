7981cdd2cb6b426838c66083c84a84aa2b38188a
Alexander Dahl <alex@dahl.dev>
2020-12-27 17:12:34 +0100

    solve day 18b

--- day18/day18.py
+++ day18/day18.py
@@ -18,13 +18,39 @@ def calc(expression):
     calculating = True
     while calculating:
         splitted = expression.split(" ")
+        print(expression)
         if len(splitted) == 1:
             return expression
-        new_expr = splitted[0] + splitted[1] + splitted[2]
-        remaining_expr = splitted[3:]
-        remaining_expr.insert(0, eval(new_expr))
-        new_str = " "
-        expression = new_str.join(map(str, remaining_expr))
+        if expression.count("+") > 0 and splitted[1] == "+":
+            new_expr = splitted[0] + splitted[1] + splitted[2]
+            remaining_expr = splitted[3:]
+            remaining_expr.insert(0, eval(new_expr))
+            new_str = " "
+            expression = new_str.join(map(str, remaining_expr))
+        elif expression.count("+") == 0:
+            new_expr = splitted[0] + splitted[1] + splitted[2]
+            remaining_expr = splitted[3:]
+            remaining_expr.insert(0, eval(new_expr))
+            new_str = " "
+            expression = new_str.join(map(str, remaining_expr))
+        elif splitted[1] == "*":
+            print("op is *")
+            # loook for next +
+            for i in range(0, len(expression) - 3):
+                if i + 1 >= len(splitted):
+                    break
+                if splitted[i + 1] == "+":
+                    new_expr = splitted[i] + splitted[i + 1] + splitted[i + 2]
+
+                    old_expr = (
+                        splitted[i] + " " + splitted[i + 1] + " " + splitted[i + 2]
+                    )
+                    print(old_expr)
+                    print(expression)
+                    expression = expression.replace(old_expr, str(eval(new_expr)))
+
+                    print(expression)
+                    break
 
 
 # print(sample_input)
@@ -45,6 +71,7 @@ for row in database:
     print(test_input)
     new_input_list = re.findall(r"\(([^()]+)\)", test_input)
     # I did not manage to make this regex myself so I stole it from reddit
+    # The rest of the code I did myself
     while new_input_list:
         print(new_input_list)
         for new_input in new_input_list:
@@ -53,8 +80,12 @@ for row in database:
             print(new_input)
             test_input = test_input.replace("(" + new_input + ")", calc_input)
         new_input_list = re.findall(r"\(([^()]+)\)", test_input)
+    # now add () around the + and do it again?
     print(test_input)
     print(calc(test_input))
     result.append(int(calc(test_input)))
 
+
 print(sum(result))
+
+# print(calc("1 + 2 * 3 + 4 * 5 + 6"))
